e4d822da447a222e26760258e719f718fa311fdf
Sidharth Gujja <sidharthgujja@Sidharths-MacBook-Pro.local>
2022-09-04 01:31:32 -0500

    added pagpes, hrefs, changed css; TODO: build information divs and key parts to each pageand use carbon

--- source/ewd.js
+++ source/ewd.js
@@ -1,19 +1,21 @@
 //client side library - no api key here
 //use endpointInit and pass the url where your endpoint.php is configured
-var endpoint;
+let endpoint;
 function endpointInit(endpointURL){
     endpoint = endpointURL;
 }
+// ugly ass js code ew so 1700 wth is this ew yucky desGUSTANG
+
 function ewdRequest(mode, document, multi, cell, data){
-    var xhttp = new XMLHttpRequest();
-    var url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
-    xhttp.open("GET", url);
-    xhttp.onreadystatechange = function(){
+    const req = new XMLHttpRequest();
+    let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
+    req.open("GET", url);
+    req.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             console.log("Request response:" + this.responseText);
             return this.responseText;
         }
-        else{
+        else {
             console.log("Request failed.");
             return "Request failed";
         }
