47e4f25d3d00c32a6da65e67cea8555591f175e6
cobbsaladluv <cobbsaladluv@gmail.com>
2022-09-03 22:39:21 -0500

    changed var to let lmao

--- source/ewd.js
+++ source/ewd.js
@@ -1,12 +1,12 @@
 //client side library - no api key here
 //use endpointInit and pass the url where your endpoint.php is configured
-var endpoint;
+let endpoint;
 function endpointInit(endpointURL){
     endpoint = endpointURL;
 }
 function ewdRequest(mode, document, multi, cell, data){
-    var xhttp = new XMLHttpRequest();
-    var url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
+    let xhttp = new XMLHttpRequest();
+    let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
     xhttp.open("GET", url);
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
