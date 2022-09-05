6536af1807f732e113da3af47d9cdb91b7f0c6aa
Sidharth Gujja <sidharthgujja@Sidharths-MacBook-Pro.local>
2022-09-04 22:56:13 -0500

    FIXED EWD.js FILE

--- source/ewd.js
+++ source/ewd.js
@@ -4,23 +4,24 @@ let endpoint;
 function endpointInit(endpointURL){
     endpoint = endpointURL;
 }
-
-function ewdRequest(mode, document, multi, cell, data){
-    let xhttp = new XMLHttpRequest();
-    let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
-    xhttp.open("GET", url);
-    xhttp.onreadystatechange = function(){
-    const req = new XMLHttpRequest();
+// ugly frickin ass js code screw you James
+// james literally from 1700's fr
+function ewdRequest(mode, document, multi, cell, data) {
+    let req = new XMLHttpRequest();
     let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
     req.open("GET", url);
-    req.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            console.log("Request response:" + this.responseText);
-            return this.responseText;
-        }
-        else {
-            console.log("Request failed.");
-            return "Request failed";
+    req.onreadystatechange = function () {
+        const req = new XMLHttpRequest();
+        let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
+        req.open("GET", url);
+        req.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log("Request response:" + this.responseText);
+                return this.responseText;
+            } else {
+                console.log("Request failed.");
+                return "Request failed";
+            }
         }
     }
 }
