fd2629b84c5d3232929ae365ceace918bf9105b4
Sidharth Gujja <sidharthgujja@Sidharths-MacBook-Pro.local>
2022-09-05 01:42:25 -0500

    bug fix w/ ewd.js

--- source/ewd.js
+++ source/ewd.js
@@ -5,33 +5,30 @@ function endpointInit(endpointURL){
     endpoint = endpointURL;
 }
 // ugly frickin ass js code screw you James
-// james literally from 1700's fr
+// James literally from 1700's fr
 
-function ewdRequest(mode, document, multi, cell, data) {
-    let req = new XMLHttpRequest();
 // ugly ass js code ew so 1700 wth is this ew yucky desGUSTANG
-
-function ewdRequest(mode, document, multi, cell, data){
-    let xhttp = new XMLHttpRequest();
-    let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
-    xhttp.open("GET", url);
-    xhttp.onreadystatechange = function(){
-    const req = new XMLHttpRequest();
-
-    let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
-    req.open("GET", url);
-    req.onreadystatechange = function () {
-        const req = new XMLHttpRequest();
+    function ewdRequest(mode, document, multi, cell, data) {
+        let xhttp = new XMLHttpRequest();
         let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
-        req.open("GET", url);
-        req.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                console.log("Request response:" + this.responseText);
-                return this.responseText;
-            } else {
-                console.log("Request failed.");
-                return "Request failed";
+        xhttp.open("GET", url);
+        xhttp.onreadystatechange = function () {
+            const req = new XMLHttpRequest();
+            let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
+            req.open("GET", url);
+            req.onreadystatechange = function () {
+                const req = new XMLHttpRequest();
+                let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
+                req.open("GET", url);
+                req.onreadystatechange = function () {
+                    if (this.readyState === 4 && this.status === 200) {
+                        console.log("Request response:" + this.responseText);
+                        return this.responseText;
+                    } else {
+                        console.log("Request failed.");
+                        return "Request failed";
+                    }
+                }
             }
         }
     }
-}
