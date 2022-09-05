3a57d23cc4c96bd5f8f34f5480cea69fe79007e9
cobbsaladluv <cobbsaladluv@gmail.com>
2022-09-04 20:36:26 -0500

    sid the js is client side let's not put swears in the code

--- source/ewd.js
+++ source/ewd.js
@@ -4,20 +4,16 @@ let endpoint;
 function endpointInit(endpointURL){
     endpoint = endpointURL;
 }
-// ugly ass js code ew so 1700 wth is this ew yucky desGUSTANG
 
 function ewdRequest(mode, document, multi, cell, data){
-<<<<<<< HEAD
     let xhttp = new XMLHttpRequest();
     let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
     xhttp.open("GET", url);
     xhttp.onreadystatechange = function(){
-=======
     const req = new XMLHttpRequest();
     let url = endpoint + "?mode=" + mode + "&document=" + document + "&multi=" + multi + "&cell=" + cell + "&data=" + data;
     req.open("GET", url);
     req.onreadystatechange = function() {
->>>>>>> e4d822da447a222e26760258e719f718fa311fdf
         if(this.readyState == 4 && this.status == 200){
             console.log("Request response:" + this.responseText);
             return this.responseText;
