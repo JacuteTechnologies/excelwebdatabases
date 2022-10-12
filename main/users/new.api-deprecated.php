196690a12308506ab52d394bead1f77fdc5babbf
cobbsaladluv <cobbsaladluv@gmail.com>
2022-10-12 11:31:00 -0500

    Major API reworks.

---
+++ main/users/new.api-deprecated.php
@@ -0,0 +1,111 @@
+<?php
+//API Merge
+require 'vendor/autoload.php';
+require '/source/BKVPlib.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+if(isset($_GET["requestType"])){
+    if($_GET["requestType"] == "cell"){
+        if(isset($_GET["key"]) && isset($_GET["mode"]) && isset($_GET["document"]) && isset($_GET["isMulti"]) && isset($_GET["cell"])){
+            if(is_dir($_GET["key"])){
+                $key = $_GET["key"];
+                $mode = $_GET["mode"];
+                $document = $_GET["document"];
+                $cell = $_GET["cell"];
+                if($mode == "r"){
+                    //read
+                    //check if file exists under api key
+                    if(file_exists($key."/".$document)){
+                        if($_GET["isMulti"] == "false"){
+                            //single cell
+                            $file = $key."/".$document;
+                            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+                            $value = $spreadsheet->getActiveSheet()->getCell($cell)->getValue();
+                            echo '{"success": true, "code": 9104, "data": '.$value.'}';
+                        }
+                        else{
+                            //multi cell
+                            if(strpos($cell, ":") !== false){
+                                $file = $key."/".$document;
+                                $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+                                $dataArray = $spreadsheet->getActiveSheet()
+                                ->rangeToArray(
+                                    $cell,
+                                    NULL,        
+                                    TRUE,       
+                                    TRUE,        
+                                    TRUE         
+                                );
+                                $j = json_encode($dataArray);
+                                echo '{"success": true, "data": '.$j.'}';
+                            }
+                            else{
+                                echo '{"success": false, "code": 9105, "data": "Invalid cell range given."}';
+                            }
+                        }
+                    }
+                    else{
+                        echo '{"success": false, "code": 9106, "data": "Document does not exist"}';
+                    }
+                }
+                else if($mode == "w"){
+                    //write
+                    //check if file exists under api key
+                    if(file_exists($key."/".$document) && isset($_GET["data"])){
+                        $data = $_GET["data"];
+                        if($_GET["isMulti"] == "false"){
+                            //single cell
+                            $file = $key."/".$document;
+                            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+                            $spreadsheet->getActiveSheet()->setCellValue($cell, $data);
+                            $writer = new Xlsx($spreadsheet);
+                            $writer->save($key."/".$document);
+                            echo '{"success": true, "code": 9107, "data": "Success"}';
+                        }
+                        else{
+                            //multi cell
+                            if(isset($_GET["data"])){
+                                $file = $key."/".$document;
+                                try{
+                                    $array = json_decode($_GET["data"]);
+                                    $success = true;
+                                }
+                                catch(Exception $e){
+                                    echo '{"success": false, "code": 9108, "data": "Invalid JSON"}';
+                                    $success = false;
+                                }
+                                if($success == true){
+                                    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+                                    $spreadsheet->getActiveSheet()->fromArray(
+                                    $array,  
+                                    NULL,      
+                                    $cell       
+                                    );
+                                    $writer = new Xlsx($spreadsheet);
+                                    $writer->save($key."/".$document);
+                                    echo '{"success": true, "code": 9107, "data": "Success"}';
+                                }
+                            }
+                            else{
+                                echo '{"success": false, "code": 9109, "data": "Data not set"}';
+                            }
+                        }
+                    }
+                    else{
+                        echo '{"success": false, "data": "Document does not exist"}';
+                    }
+                }
+            }
+            else{
+                echo '{"success":false, "code":9103, "data":"Invalid key."';
+            }
+        }
+        else{
+            echo '{"success":false, "code":9102, "data":"Missing parameter."';
+        }
+    }
+}
+else{
+    echo '{"success":false, "code":9100, "data":"No request type given."';
+}
+?>
