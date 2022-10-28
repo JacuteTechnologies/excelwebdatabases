3e0e2edd8e04495446af54c1fe867b0ae9880574
voidSevenSevenSix <cobbsaladluv@gmail.com>
2022-10-28 11:48:30 -0500

    API should be fixed now.

--- main/users/APIv4.php
+++ main/users/APIv4.php
@@ -5,16 +5,58 @@ require '/source/BKVPlib.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 function readExcelCell($document, $cell){
-
+    $key = $_GET["key"];
+    $file = $key."/".$document;
+    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+    $value = $spreadsheet->getActiveSheet()->getCell($cell)->getValue();
+    echo '{"success": true, "code": 9104, "data": '.$value.'}';
 }
 function readExcelCellMulti($document, $cell){
-
+    $key = $_GET["key"];
+    $file = $key."/".$document;
+    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+    $dataArray = $spreadsheet->getActiveSheet()
+    ->rangeToArray(
+        $cell,
+        NULL,        
+        TRUE,       
+        TRUE,        
+        TRUE         
+    );
+    $j = json_encode($dataArray);
+    echo '{"success": true, "data": '.$j.'}';
 }
 function writeExcelCell($document, $cell, $data){
-
+    $key = $_GET["key"];
+    $file = $key."/".$document;
+    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+    $spreadsheet->getActiveSheet()->setCellValue($cell, $data);
+    $writer = new Xlsx($spreadsheet);
+    $writer->save($key."/".$document);
+    echo '{"success": true, "code": 9107, "data": "Success"}';
 }
 function writeExcelCellMulti($document, $cell, $data){
-
+    $key = $_GET["key"];
+    $file = $key."/".$document;
+    try{
+        $array = json_decode($_GET["data"]);
+        $success = true;
+    }
+    catch(Exception $e){
+        echo '{"success": false, "code": 9108, "data": "Invalid JSON"}';
+        $success = false;
+    }
+    if($success == true){
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+        $spreadsheet->getActiveSheet()->fromArray(
+        $array,  
+        NULL,      
+        $cell       
+        );
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($key."/".$document);
+        echo '{"success": true, "code": 9107, "data": "Success"}';
+    }
 }
 if(isset($_GET["key"]) && isset($_GET["mode"]) && isset($_GET["document"]) && isset($_GET["isMulti"])){
     if(is_dir($_GET["key"])){
